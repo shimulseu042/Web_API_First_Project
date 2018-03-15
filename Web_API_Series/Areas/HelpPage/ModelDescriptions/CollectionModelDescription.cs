5fbb358b7ff9517f29cea29dc52419d19ab1dbf4
S.M. Giyasuddin <shimul.cse012@gmail.com>
2018-03-15 20:08:07 +0600

    First Web Api Commit

---
+++ Web_API_Series/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Web_API_Series.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Web_API_Series/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Web_API_Series.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
